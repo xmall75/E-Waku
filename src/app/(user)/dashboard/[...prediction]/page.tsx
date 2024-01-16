367eebd43ae8620edda59f65df4110341e9454c8
Revanantyo D <xmall75.id@gmail.com>
2024-01-16 13:57:11 +0700

    integrated prediction system & ui

--- src/app/(user)/dashboard/[...prediction]/page.tsx
+++ src/app/(user)/dashboard/[...prediction]/page.tsx
@@ -17,98 +17,140 @@ export default function PredictionPage(
     const { params } = props
     const router = useRouter()
 
-    const [prediction, setPrediction] = useState(0)
+    const [ predictionScore, setPredictionScore ] = useState<any>()
+    const [ isLoading, setIsLoading ] = useState(false)
 
-    let fish_gurame, fish_lele, fish_nila,
-        land_type_beton, land_type_ember,
-        land_type_terpal, food_type_maggot,
-        food_type_pelet
 
-    if(params.prediction[1]) {
-        if(params.prediction[1] === 'catfish') {
-            fish_gurame = 0
-            fish_nila = 0
-            fish_lele = 1
-        }
-        else if(params.prediction[1] === 'gourame') {
-            fish_gurame = 1
-            fish_nila = 0
-            fish_lele = 0
-        }
-        else if(params.prediction[1] === 'parrot') {
-            fish_gurame = 0
-            fish_nila = 1
-            fish_lele = 0
-        }
-        else {
-            router.push('/dashboard/prediction')
-        }
+    let fish_gurame: any, fish_lele: any, fish_nila: any,
+        land_type_beton: any, land_type_ember: any,
+        land_type_terpal: any, food_type_maggot: any,
+        food_type_pelet: any, feeding_cycle: any,
+        land_area: any, seeds: any, budget: any
+
+    const handleLandArea = (e: any) => {
+        e.preventDefault()
+
+        router.push(`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${e.target.land_area.value}`)
     }
 
-    if(params.prediction[2]) {
-        if(params.prediction[2] === 'terpaulin') {
-            land_type_beton = 0
-            land_type_ember = 0
-            land_type_terpal = 1
-        }
-        else if(params.prediction[2] === 'concrete') {
-            land_type_beton = 1
-            land_type_ember = 0
-            land_type_terpal = 0
-        }
-        else if(params.prediction[2] === 'bucket') {
-            land_type_beton = 0
-            land_type_ember = 1
-            land_type_terpal = 0
-        }
-        else {
-            router.push('/dashboard/prediction')
-        }
+    const handleBudget = (e: any) => {
+        e.preventDefault()
+
+        router.push(`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/${params.prediction[5]}/${e.target.budget.value}`)
     }
-
-
 
     const handleSubmit = async (e: any) => {
         e.preventDefault()
+
+        setIsLoading(true)
+
+        if(params.prediction[1]) {
+            if(params.prediction[1] === 'catfish') {
+                fish_gurame = 0
+                fish_nila = 0
+                fish_lele = 1
+            }
+            else if(params.prediction[1] === 'gourame') {
+                fish_gurame = 1
+                fish_nila = 0
+                fish_lele = 0
+            }
+            else if(params.prediction[1] === 'parrot') {
+                fish_gurame = 0
+                fish_nila = 1
+                fish_lele = 0
+            }
+            else {
+                router.push('/dashboard/prediction')
+            }
+        }
+    
+        if(params.prediction[2]) {
+            if(params.prediction[2] === 'terpaulin') {
+                land_type_beton = 0
+                land_type_ember = 0
+                land_type_terpal = 1
+            }
+            else if(params.prediction[2] === 'concrete') {
+                land_type_beton = 1
+                land_type_ember = 0
+                land_type_terpal = 0
+            }
+            else if(params.prediction[2] === 'bucket') {
+                land_type_beton = 0
+                land_type_ember = 1
+                land_type_terpal = 0
+            }
+            else {
+                router.push('/dashboard/prediction')
+            }
+        }
+
+        if(params.prediction[3]) {
+            land_area = params.prediction[3]
+        }
+    
+        if(params.prediction[4]) {
+            if(params.prediction[4] === 'pellet') {
+                food_type_maggot = 0
+                food_type_pelet = 1
+            }
+            else if(params.prediction[4] === 'maggot') {
+                food_type_maggot = 1
+                food_type_pelet = 0
+            }
+            else {
+                router.push('/dashboard/prediction')
+            }
+        }
+    
+        if(params.prediction[5]) {
+            if(params.prediction[5] === '2') {
+                feeding_cycle = 2
+            }
+            else if(params.prediction[5] === '3') {
+                feeding_cycle = 3
+            }
+            else {
+                router.push('/dashboard/prediction')
+            }
+        }
+
+        if(params.prediction[6]) {
+            budget = params.prediction[6]
+        }
         
         const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/prediction`, {
 			method: 'POST',
 			body: JSON.stringify({
-                land_area: 40,
-                total_seeds: 500,
-                budget: 500000,	
-                feeding_cycle: 2,
-                fish_gurame: 0,
-                fish_lele: 1,
-                fish_nila: 0,
-                land_type_beton: 0,
-                land_type_ember: 1,
-                land_type_terpal: 0,
-                food_type_maggot: 0,
-                food_type_pelet: 1
-				// land_area: e.target.land_area.value,
-                // total_seeds: e.target.total_seeds.value,
-                // budget: e.target.budget.value,	
-                // feeding_cycle: e.target.feeding_cycle.value,
-                // fish_gurame: e.target.fish_gurame.value,
-                // fish_lele: e.target.fish_lele.value,
-                // fish_nila: e.target.fish_nila.value,
-                // land_type_beton: e.target.land_type_beton.value,
-                // land_type_ember: e.target.land_type_ember.value,
-                // land_type_terpal: e.target.land_type_terpal.value,
-                // food_type_maggot: e.target.food_type_maggot.value,
-                // food_type_pelet: e.target.food_type_pelet.value
+				land_area: land_area,
+                total_seeds: e.target.seeds.value,
+                budget: budget,	
+                feeding_cycle: feeding_cycle,
+                fish_gurame: fish_gurame,
+                fish_lele: fish_lele,
+                fish_nila: fish_nila,
+                land_type_beton: land_type_beton,
+                land_type_ember: land_type_ember,
+                land_type_terpal: land_type_terpal,
+                food_type_maggot: food_type_maggot,
+                food_type_pelet: food_type_pelet
 			})
 		})
         if(res.status === 200) {
             console.log(res.json().then((data) => {
                 console.log(data)
-                setPrediction(data.data)
+
+                setPredictionScore(data.data)
+                setIsLoading(false)
+
+                // router.push(`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/${params.prediction[5]}/${params.prediction[6]}/${e.target.seeds.value}/result`)
             }))
         }
         else {
             console.log(res.json().then((data) => {
                 console.log(data)
+                setIsLoading(false)
             }))
         }
     }
@@ -155,7 +197,12 @@ export default function PredictionPage(
                         <h3 className="font-[Poppins] font-semibold text-xl mb-8">What is the size of your fish pond?</h3>
                         <h6 className="font-[Poppins] text-sm max-w-lg my-8">The size of the pond you choose will determine the number of fish you can cultivate.</h6>
                         
-                        <div className="flex items-center justify-center w-full h-20 bg-[#E3EDF8] font-semibold my-5">Fill here in (m^2)</div>
+                        <form method="post" onSubmit={(e) => (handleLandArea(e))}>
+                            <div className="flex items-center justify-right w-full h-20 bg-[#E3EDF8] font-semibold my-5 pr-5 font-semibold">
+                                <input name="land_area" type="text" className="flex items-center justify-left w-full h-20 bg-[#E3EDF8] font-semibold my-5 mr-5 pl-3 font-semibold" placeholder="Pond size (m^2)" />
+                                <button type="submit"><Image src="/pictures/kuis-next.png" alt="next button" width={73} height={73} /></button>
+                            </div>
+                        </form>      
                     </div>
                     </>
                 )}
@@ -180,8 +227,8 @@ export default function PredictionPage(
                         <h3 className="font-[Poppins] font-semibold text-xl mb-8">How often do you feed your fish in a day?</h3>
                         <h6 className="font-[Poppins] text-sm max-w-lg my-8">The feeding frequency you choose will affect the cost of fish farming.</h6>
                         
-                        <Link href={`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/2`}></Link><div className="flex items-center justify-center w-full h-20 bg-[#E3EDF8] font-semibold my-5">2x</div>
-                        <Link href={`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/3`}></Link><div className="flex items-center justify-center w-full h-20 bg-[#E3EDF8] font-semibold my-5">3x</div>
+                        <Link href={`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/2`}><div className="flex items-center justify-center w-full h-20 bg-[#E3EDF8] font-semibold my-5">2x</div></Link>
+                        <Link href={`/dashboard/prediction/${params.prediction[1]}/${params.prediction[2]}/${params.prediction[3]}/${params.prediction[4]}/3`}><div className="flex items-center justify-center w-full h-20 bg-[#E3EDF8] font-semibold my-5">3x</div></Link>
                     </div>
                     </>
                 )}
@@ -192,8 +239,37 @@ export default function PredictionPage(
                     <div className="w-full py-12 pl-28">
                         <h3 className="font-[Poppins] font-semibold text-xl mb-8">How much money will you spend for this fish farming project?</h3>
                         <h6 className="font-[Poppins] text-sm max-w-lg my-8">Your budget will determine all aspects of your fish farming, from the type of fish to the type of feed.</h6>
+
+                        <form method="post" onSubmit={(e) => (handleBudget(e))}>
+                            <div className="flex items-center justify-right w-full h-20 bg-[#E3EDF8] font-semibold my-5 pl-10 pr-5 font-semibold">Rp 
+                                <input name="budget" type="text" className="flex items-center justify-left w-full h-20 bg-[#E3EDF8] font-semibold my-5 ml-7 mr-5 pl-3 font-semibold" placeholder="Your budget here" />
+                                <button type="submit"><Image src="/pictures/kuis-next.png" alt="next button" width={73} height={73} /></button>
+                            </div>
+                        </form>    
+                    </div>
+                    </>
+                )}
+
+                {!params.prediction[7] && params.prediction[6] && (
+                    <>
+                    <Image alt="banner" src="/pictures/kuis-banner-6.png" width={400} height={598} />
+                    <div className="w-full py-12 pl-28">
+                        <h3 className="font-[Poppins] font-semibold text-xl mb-8">How many fish seeds would you like for your farm?</h3>
+                        <h6 className="font-[Poppins] text-sm max-w-lg my-8">The total fish seeds will affect on your feed levels.</h6>
                         
-                        <div className="flex items-center justify-left w-full h-20 bg-[#E3EDF8] font-semibold my-5 pl-10 font-semibold">Rp</div>
+                        <form method="post" onSubmit={(e) => (handleSubmit(e))}>
+                            <div className="flex items-center justify-right w-full h-20 bg-[#E3EDF8] font-semibold my-5 pr-5 font-semibold">
+                                <input name="seeds" type="text" className="flex items-center justify-left w-full h-20 bg-[#E3EDF8] font-semibold my-5 mr-5 pl-3 font-semibold" placeholder="Total fish seeds" />
+                                <button type="submit"><Image src="/pictures/kuis-next.png" alt="next button" width={73} height={73} /></button>
+                            </div>
+                        </form>
+
+                        {isLoading ? ('Please wait...') : (predictionScore && 
+                        <>
+                            <h3 className="font-[Poppins] font-semibold text-xl mt-10">{predictionScore.result && <>Your success probability is: {predictionScore.result.toUpperCase()}</>}</h3>
+                            <h6 className="font-[Poppins] text-sm max-w-lg my-1">{predictionScore.score && <>This prediction is {(predictionScore.score * 100).toFixed(2)}% accurate</>}</h6>
+                        </>)
+                        }
                     </div>
                     </>
                 )}
